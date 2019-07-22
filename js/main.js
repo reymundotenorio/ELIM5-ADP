4ec00cf0cca247adcb3161894ec0fdb1e0fa0888
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-07-22 10:58:55 -0700

    Buildings background animation finished

--- js/main.js
+++ js/main.js
@@ -4,20 +4,88 @@ $(document).ready(function() {
   // Move buildings functions
   function moveBuildingsBackground() {
     tweenMaxRepeat
-      .to("#buildings", 4, {
-        backgroundPosition: "75% 0%",
+      .to("#buildings", 1, {
+        backgroundPosition: "30% 100%",
         ease: Linear.easeNone
       })
       .to("#buildings", 2, {
-        backgroundPosition: "25% 0%",
+        // Return 0
+        backgroundPosition: "0% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 3, {
+        // Delay on 0
+        backgroundPosition: "0% 100%",
         ease: Linear.easeNone
       })
       .to("#buildings", 1, {
-        backgroundPosition: "100% 0%",
+        backgroundPosition: "50% 100%",
         ease: Linear.easeNone
       })
-      .to("#buildings", 5, {
-        backgroundPosition: "0% 0%",
+      .to("#buildings", 0.8, {
+        backgroundPosition: "48% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 1, {
+        backgroundPosition: "60% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 1.2, {
+        backgroundPosition: "48% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 0.2, {
+        backgroundPosition: "48% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 1, {
+        backgroundPosition: "50% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 0.5, {
+        backgroundPosition: "50% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 1, {
+        backgroundPosition: "60% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 0.2, {
+        // Delay on 60
+        backgroundPosition: "60% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 1, {
+        backgroundPosition: "80% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 1, {
+        backgroundPosition: "78% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 1, {
+        backgroundPosition: "82% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 1, {
+        backgroundPosition: "75% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 1, {
+        backgroundPosition: "98% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 1.5, {
+        backgroundPosition: "35% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 1, {
+        // Delay on 35
+        backgroundPosition: "35% 100%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 1.2, {
+        backgroundPosition: "0% 100%",
         ease: Linear.easeNone
       });
   }
