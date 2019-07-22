f115c4e3f8589d9ef973d6e1d2e9753957d50747
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-07-22 05:56:09 -0700

    Fixing Maximum Call Stack Size Exceeded

--- js/main.js
+++ js/main.js
@@ -1,29 +1,27 @@
-$( document ).ready(function() {
-  var tweenMax = new TimelineMax();
+$(document).ready(function() {
+  var tweenMaxRepeat = new TimelineMax({ repeat: -1 });
 
+  // Move buildings functions
   function moveBuildingsBackground() {
-    tweenMax.to("#buildings", 4, {
-      backgroundPosition: "75% 0%",
-      ease: Linear.easeNone
-    });
-  
-    tweenMax.to("#buildings", 2, {
-      backgroundPosition: "25% 0%",
-      ease: Linear.easeNone
-    });
-  
-    tweenMax.to("#buildings", 1, {
-      backgroundPosition: "100% 0%",
-      ease: Linear.easeNone
-    });
-  
-    tweenMax.to("#buildings", 5, {
-      backgroundPosition: "0% 0%",
-      ease: Linear.easeNone
-    });
-  
-    moveBuildingsBackground();
+    tweenMaxRepeat
+      .to("#buildings", 4, {
+        backgroundPosition: "75% 0%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 2, {
+        backgroundPosition: "25% 0%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 1, {
+        backgroundPosition: "100% 0%",
+        ease: Linear.easeNone
+      })
+      .to("#buildings", 5, {
+        backgroundPosition: "0% 0%",
+        ease: Linear.easeNone
+      });
   }
-  
+
+  // Run move buildings functions
   moveBuildingsBackground();
 });
